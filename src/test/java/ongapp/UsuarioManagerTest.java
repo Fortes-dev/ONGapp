0a19af6ccc7bd5c94211822ef7b3c4f2694a7039
NullAlu <74355920+NullAlu@users.noreply.github.com>
2021-05-19 22:59:47 +0200

    Tests OngController

--- src/test/java/ongapp/UsuarioManagerTest.java
+++ src/test/java/ongapp/UsuarioManagerTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
@@ -16,31 +17,47 @@ import ongapp.dao.Usuario;
 import ongapp.manager.UsuarioManager;
 
 class UsuarioManagerTest {
-
 	private Connection con;
-	
+
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() throws SQLException, ClassNotFoundException {
 		con = new Conector().getMySQLConnection();
 		con.createStatement().execute("use ongapp");
-	}
-	
-	@Test
-	void findAll_ok() {
-		List<Usuario> usuarios = new UsuarioManager().findAll(con);
-		assertTrue(usuarios.size()>0);
-		assertFalse(usuarios.isEmpty());
-	}
-	
-	@Test
-	void findLoginUser_ok() {
-		String user =  new UsuarioManager().findLoginUser(con, "carlos");
-		assertEquals("carlos", user);
-	}
-	
-	@AfterEach
-	void tearDown() throws Exception {
-		con.close();
+		con.createStatement().execute("INSERT INTO `ongapp`.`usuario` (`id`, `email`, `username`, `contraseña`, `rol`) "
+				+ "VALUES ('132', 'prueba@gmail.com', 'prueba', 'prueba', 'admin')");
 	}
 
+	@Test
+	void findAll_ok() throws SQLException {
+		List<Usuario> usuarios = new UsuarioManager().findAll(con);
+		assertTrue(usuarios.size() > 0);
+		assertFalse(usuarios.isEmpty());
+	}
+
+	@Test
+	void findLoginUser_ok() throws SQLException {
+		String user = new UsuarioManager().findLoginUser(con, "prueba");
+		assertEquals("prueba", user);
+		assertFalse(user.isEmpty());
+	}
+
+	@Test
+	void findLoginPassword_ok() throws SQLException {
+		String user = new UsuarioManager().findLoginPassword(con, "prueba");
+		assertEquals("prueba", user);
+		assertFalse(user.isEmpty());
+	}
+
+	@Test
+	void findLoginRol_ok() throws SQLException {
+		String user = new UsuarioManager().findLoginRol(con, "prueba");
+		assertEquals("admin", user);
+		assertFalse(user.isEmpty());
+	}
+
+	@AfterEach
+	void tearDown() throws SQLException {
+		con.createStatement().execute("DELETE FROM usuario WHERE username = 'prueba'");
+		con.close();
+	}
 }
